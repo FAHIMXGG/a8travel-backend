82de69021b04ce0c3c42f9f0b24e84a9d44ec6ab
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-12-03 13:07:36 +0600

    for vercel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import app from "./app";
 import { env } from "./config/env";
 
 app.listen(env.PORT, () => {
   console.log(`Server running on http://localhost:${env.PORT}`);
 });
+
+// This converts Express app to a handler compatible with Vercel
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  // @ts-ignore
+  return app(req, res);
+}
